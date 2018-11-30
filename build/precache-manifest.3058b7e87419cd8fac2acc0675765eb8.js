a29278c06bf53964f0766c6fce6351dfb843f55a
jucai <jucai@github.com>
2018-11-29 23:03:37 -0800

    ADD: [11.29] add new build file

---
+++ build/precache-manifest.3058b7e87419cd8fac2acc0675765eb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c74ea7dcaaba83a3a994591771937b68",
+    "url": "/static/media/background.c74ea7dc.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13dd73da70983b327bf1",
+    "url": "/static/js/main.13dd73da.chunk.js"
+  },
+  {
+    "revision": "a25fc459400be5372294",
+    "url": "/static/js/1.a25fc459.chunk.js"
+  },
+  {
+    "revision": "13dd73da70983b327bf1",
+    "url": "/static/css/main.1f3c350a.chunk.css"
+  },
+  {
+    "revision": "a25fc459400be5372294",
+    "url": "/static/css/1.2125ef10.chunk.css"
+  },
+  {
+    "revision": "99247a4460141e948aee67987929f44e",
+    "url": "/index.html"
+  }
+];
