82b029cbfaa3f4c34fe0666720624faaa12ec459
jucai <jucai@github.com>
2018-12-12 15:19:38 -0800

    FIX: [12.12] fix table display

--- src/components/PlayerDataTable.js
+++ src/components/PlayerDataTable.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Row, Col, Table, Radio, Spin } from "antd";
-import { observer, inject, Provider } from "mobx-react";
+import { Table, Radio, Spin } from "antd";
+import { observer, inject } from "mobx-react";
 
 class PlayerDataTable extends React.Component {
   state = {
@@ -16,121 +16,146 @@ class PlayerDataTable extends React.Component {
   };
 
   render() {
+    const columnWidth = 60;
     const columns = [
       {
         title: "StatType",
         dataIndex: "statType",
         key: "statType",
         filteredValue: [this.state.seasonSelect],
-        onFilter: (value, record) => record.statType.includes(value)
+        onFilter: (value, record) => record.statType.includes(value),
+        fixed: "left"
       },
       {
         title: "Season",
+        width: 100,
         dataIndex: "seasonID",
-        key: "seasonID"
+        key: "seasonID",
+        fixed: "left"
       },
       {
         title: "Team",
+        width: columnWidth,
         dataIndex: "teamAbbreviation",
         key: "teamAbbreviation"
       },
       {
         title: "Played",
+        width: columnWidth,
         dataIndex: "gamePlayed",
         key: "gamePlayed"
       },
       {
         title: "MIN",
+        width: columnWidth,
         dataIndex: "minute",
         key: "minute"
       },
       {
         title: "FGM",
+        width: columnWidth,
         dataIndex: "fieldGoalsMade",
         key: "fieldGoalsMade"
       },
       {
         title: "FGA",
+        width: columnWidth,
         dataIndex: "fieldGoalsAttempted",
         key: "fieldGoalsAttempted"
       },
       {
         title: "FGP",
+        width: columnWidth,
         dataIndex: "fieldGoalPercentage",
         key: "fieldGoalPercentage"
       },
       {
         title: "3PM",
+        width: columnWidth,
         dataIndex: "threePointFieldGoalsMade",
         key: "threePointFieldGoalsMade"
       },
       {
         title: "3PA",
+        width: columnWidth,
         dataIndex: "threePointFieldGoalsAttempted",
         key: "threePointFieldGoalsAttempted"
       },
       {
         title: "3P%",
+        width: columnWidth,
         dataIndex: "threePointFieldGoalsPercentage",
         key: "threePointFieldGoalsPercentage"
       },
       {
         title: "FTM",
+        width: columnWidth,
         dataIndex: "freeThrowsMade",
         key: "freeThrowsMade"
       },
       {
         title: "FTA",
+        width: columnWidth,
         dataIndex: "freeThrowsAttempted",
         key: "freeThrowsAttempted"
       },
       {
         title: "FTP",
+        width: columnWidth,
         dataIndex: "freeThrowPercentage",
         key: "freeThrowPercentage"
       },
       {
         title: "OREB",
+        width: columnWidth,
         dataIndex: "offensiveRebounds",
         key: "offensiveRebounds"
       },
       {
         title: "DREB",
+        width: columnWidth,
         dataIndex: "defensiveRebounds",
         key: "defensiveRebounds"
       },
       {
         title: "REB",
+        width: columnWidth,
         dataIndex: "Rebounds",
         key: "Rebounds"
       },
       {
         title: "AST",
+        width: columnWidth,
         dataIndex: "Assists",
         key: "Assists"
       },
       {
         title: "TO",
+        width: columnWidth,
         dataIndex: "blocks",
         key: "blocks"
       },
       {
         title: "STL",
+        width: columnWidth,
         dataIndex: "turnovers",
         key: "turnovers"
       },
       {
         title: "BLK",
+        width: columnWidth,
         dataIndex: "steals",
         key: "steals"
       },
       {
         title: "PF",
+        width: columnWidth,
         dataIndex: "personalFouls",
         key: "personalFouls"
       },
       {
         title: "PTS",
+        width: columnWidth,
         dataIndex: "Points",
         key: "Points"
       }
@@ -158,6 +183,7 @@ class PlayerDataTable extends React.Component {
             pagination={false}
             columns={columns}
             dataSource={this.props.store.playerStatistic}
+            scroll={{ x: 1540 }}
           />
         </div>
       </div>
--- src/components/TeamTraditionalStat.js
+++ src/components/TeamTraditionalStat.js
@@ -17,6 +17,8 @@ class TeamTraditionalStat extends React.Component {
   };
 
   render() {
+    const columnWidth = 60;
+
     const columns = [
       {
         title: "GroupValue",
@@ -29,131 +31,157 @@ class TeamTraditionalStat extends React.Component {
       },
       {
         title: "GP",
+        width: columnWidth,
         dataIndex: "gamePlayed",
         key: "gamePlayed"
       },
       {
         title: "Win",
+        width: columnWidth,
         dataIndex: "win",
         key: "win"
       },
       {
         title: "Lose",
+        width: columnWidth,
         dataIndex: "lose",
         key: "lose"
       },
       {
         title: "WP",
+        width: columnWidth,
         dataIndex: "winPercentage",
         key: "winPercentage"
       },
       {
         title: "MIN",
+        width: columnWidth,
         dataIndex: "minute",
         key: "minute"
       },
       {
         title: "FGM",
+        width: columnWidth,
         dataIndex: "fieldGoalsMade",
         key: "fieldGoalsMade"
       },
       {
         title: "FGA",
+        width: columnWidth,
         dataIndex: "fieldGoalsAttempted",
         key: "fieldGoalsAttempted"
       },
       {
         title: "FGP",
+        width: columnWidth,
         dataIndex: "fieldGoalPercentage",
         key: "fieldGoalPercentage"
       },
       {
         title: "3PM",
+        width: columnWidth,
         dataIndex: "threePointFieldGoalsMade",
         key: "threePointFieldGoalsMade"
       },
       {
         title: "3PA",
+        width: columnWidth,
         dataIndex: "threePointFieldGoalsAttempted",
         key: "threePointFieldGoalsAttempted"
       },
       {
         title: "3P%",
+        width: columnWidth,
         dataIndex: "threePointFieldGoalsPercentage",
         key: "threePointFieldGoalsPercentage"
       },
       {
         title: "FTM",
+        width: columnWidth,
         dataIndex: "freeThrowsMade",
         key: "freeThrowsMade"
       },
       {
         title: "FTA",
+        width: columnWidth,
         dataIndex: "freeThrowsAttempted",
         key: "freeThrowsAttempted"
       },
       {
         title: "FTP",
+        width: columnWidth,
         dataIndex: "freeThrowPercentage",
         key: "freeThrowPercentage"
       },
       {
         title: "OREB",
+        width: columnWidth,
         dataIndex: "offensiveRebounds",
         key: "offensiveRebounds"
       },
       {
         title: "DREB",
+        width: columnWidth,
         dataIndex: "defensiveRebounds",
         key: "defensiveRebounds"
       },
       {
         title: "REB",
+        width: columnWidth,
         dataIndex: "rebounds",
         key: "rebounds"
       },
       {
         title: "AST",
+        width: columnWidth,
         dataIndex: "asists",
         key: "asists"
       },
       {
         title: "TOV",
+        width: columnWidth,
         dataIndex: "turnovers",
         key: "turnovers"
       },
       {
         title: "STL",
+        width: columnWidth,
         dataIndex: "steals",
         key: "steals"
       },
       {
         title: "BLK",
+        width: columnWidth,
         dataIndex: "blocks",
         key: "blocks"
       },
       {
         title: "BFGA",
+        width: columnWidth,
         dataIndex: "blockFieldGoalsAttempted",
         key: "blockFieldGoalsAttempted"
       },
       {
         title: "PF",
+        width: columnWidth,
         dataIndex: "personalFouls",
         key: "personalFouls"
       },
       {
         title: "PFD",
+        width: columnWidth,
         dataIndex: "personalFoulsDrawn",
         key: "personalFoulsDrawn"
       },
       {
         title: "PTS",
+        width: columnWidth,
         dataIndex: "points",
         key: "points"
       },
       {
         title: "+/-",
+        width: columnWidth,
         dataIndex: "plusMinus",
         key: "plusMinus"
       }
@@ -172,11 +200,11 @@ class TeamTraditionalStat extends React.Component {
           </Radio.Group>
         </div>
         <Table
-          size={"small"}
+          size={"middle"}
           pagination={false}
           columns={columns}
           dataSource={this.props.store.team.traditionalStat}
-          scroll={{ x: true }}
+          scroll={{ x: 1670 }}
         />
       </div>
     );
